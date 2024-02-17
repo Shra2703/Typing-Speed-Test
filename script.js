15f136f7db6ea9bb8916e186410191b2a1353ba4
shraddha goyal <shraddhagoyal800@gmail.com>
2024-02-17 17:51:46 +0530

    Logic of typing speed test is completed

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+
+// sentence which have to display
 let sentences = `Often called Lord Krishna, he is one of the most widely worshiped and popular Hindu deities. Krishna is the eighth avatar (or incarnation) of Vishnu. He was deified in the 5th century and since then has also been worshiped as the supreme god himself. Krishna is the Hindu god of compassion, protection, and love.
 `;
+
+
 let timeInterval = "";
-let timerDuration = 10;
+let timerDuration = 30;
 
 const sentenceEle = document.getElementById("sentence");
 const startBtnEle = document.getElementById("start-btn");
@@ -13,13 +17,22 @@ const accuracyEle = document.getElementById("accuracy");
 const retryEle = document.getElementById("retry-btn");
 
 
+// to disble copy paste on the document
+document.addEventListener("copy" , (e) => {
+  e.preventDefault();
+}, false)
 
+document.addEventListener("contextmenu" , (e) => {
+  e.preventDefault();
+}, false)
+
+// Start btn function
 startBtnEle.addEventListener("click", startFunc);
 
 function startFunc() {
   sentenceEle.innerHTML = sentences;
   startBtnEle.disabled = true;
-  inputEle.disabled = false;
+  // inputEle.disabled = false;
   inputEle.focus();
   startTimer();
   // setTimeout(() => {
@@ -27,16 +40,24 @@ function startFunc() {
   // }, 30000)
 }
 
+// input function disbled
+function inputFunction() {
+  inputEle.disabled = false;
+}
+
+// function to start timer
 function startTimer() {
   timeInterval = setInterval(() => {
     timerEle.textContent = `0:${timerDuration} sec`;
     timerDuration--;
+    inputFunction();
     if (timerDuration <= -1) {
       endGame();
     }
   }, 1000);
 }
 
+// end game function where the logic of how speed and accuracy test
 function endGame() {
   clearInterval(timeInterval);
   let typedSentence = inputEle.value.trim();
@@ -65,5 +86,15 @@ function endGame() {
   resultEle.style.display = "block";
   inputEle.value = "";
   inputEle.disabled = true;
+  retryEle.focus();
   console.log(correctWordCount);
 }
+
+// retry btn element
+retryEle.addEventListener("click", () => {
+  resultEle.style.display = "none";
+  startBtnEle.disabled = false;
+  timerDuration =  30;
+  inputEle.focus();
+
+});
